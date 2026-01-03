eda3d7d699da098c61b8ea4d4f65777a70b54215
Glenn Reckard <myaimentor03@gmail.com>
2026-01-02 23:14:59 -0800

    Allow frontend origin via CORS

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,19 @@
-﻿import express, { type Request, Response, NextFunction } from "express";
+﻿import cors from "cors";
+import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    origin: "https://mechaniceye.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use((req, res, next) => {
   const start = Date.now();
