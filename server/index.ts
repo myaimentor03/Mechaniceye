5656f021c93230ce8e1c52ea8475ad24cf34284e
Glenn Reckard <myaimentor03@gmail.com>
2026-03-06 19:50:43 -0800

    Remove serveStatic for Render backend

--- server/index.ts
+++ server/index.ts
@@ -53,8 +53,7 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
   // await setupVite(app, server);
   } else {
-    serveStatic(app);
-  }
+     }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
