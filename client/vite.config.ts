a4ea1a0abfc1f723b8b064f35f6e61edc8393e90
Glenn Reckard <myaimentor03@gmail.com>
2026-03-08 13:22:52 -0700

    Fix Vite root and output directory

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname),
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+  build: {
+    outDir: path.resolve(__dirname, "../dist"),
+    emptyOutDir: true,
   },
 });
