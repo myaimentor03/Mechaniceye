c74c8e64ae8a00741608a8125bc19ab2b92fd654
Glenn Reckard <myaimentor03@gmail.com>
2026-03-08 22:21:49 -0700

    Fix frontend build output for Render

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  root: path.resolve(__dirname),
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, "../dist"),
-    emptyOutDir: true,
-  },
+    outDir: "dist",
+    emptyOutDir: true
+  }
 });
